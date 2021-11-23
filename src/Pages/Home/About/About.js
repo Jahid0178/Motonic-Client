96c95b8cb3a6fcdeda039a16ac880293c55468f5
Jahid <jahidulhaque767@gmail.com>
2021-11-23 19:27:42 +0600

    added about

--- src/Pages/Home/About/About.js
+++ src/Pages/Home/About/About.js
@@ -1,11 +1,43 @@
+import { Typography, Box, Grid, Button } from "@mui/material";
 import React from "react";
+import AboutImg from "../../../Images/Home/about-pic.png";
 import "./About.css";
+
+const alignment = {
+  display: "flex",
+  alignItems: "center",
+};
 
 const About = () => {
   return (
-    <div>
-      <h2>This is home about</h2>
-    </div>
+    <Box>
+      <Grid container spacing={2}>
+        <Grid xs={12} sm={12} md={6}>
+          <img src={AboutImg} alt="" style={{ width: "80%" }} />
+        </Grid>
+        <Grid xs={12} sm={12} md={6} style={alignment}>
+          <Box>
+            <Typography sx={{ mb: 1 }}>About Our Company</Typography>
+            <Typography variant="h4">How We Can Help you</Typography>
+            <Typography sx={{ my: 4 }}>
+              There are many variations of passages of Lorem Ipsum available,
+              but the majority have suffered alteration in some form, by
+              injected humour, or randomised words which don't look even
+              slightly believable. If you are going to use a passage of Lorem
+              Ipsum, you need to be sure there.
+            </Typography>
+            <Typography>
+              All the Lorem Ipsum generators on the Internet tend to repeat
+              predefined chunks as necessary anything embarrassing hidden in
+              the.
+            </Typography>
+            <Button variant="contained" sx={{ mt: 5 }}>
+              Learn More
+            </Button>
+          </Box>
+        </Grid>
+      </Grid>
+    </Box>
   );
 };
 
