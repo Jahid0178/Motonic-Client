5c50cb6fcd101dc89d2a7e95b48e07ba433f993e
Jahid <jahidulhaque767@gmail.com>
2021-11-25 02:34:36 +0600

    complete service design

--- src/Pages/Services/Service/Service.js
+++ src/Pages/Services/Service/Service.js
@@ -4,23 +4,33 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
+import "./Service.css";
+import { Box } from "@mui/material";
 
 const Service = ({ service }) => {
   const { img, title, desc } = service;
   return (
     <Grid item xs={12} sm={12} md={4}>
-      <Card sx={{ maxWidth: 345, py: 3, textAlign: "center" }}>
-        <CardMedia
-          component="img"
-          sx={{ width: 50, margin: "auto" }}
-          image={img}
-          alt="green iguana"
-        />
+      <Card
+        sx={{ maxWidth: 345, py: 3, textAlign: "center", boxShadow: "none" }}
+      >
+        <Box className="service-img">
+          <CardMedia
+            component="img"
+            sx={{ width: 50, margin: "auto" }}
+            image={img}
+            alt={title}
+          />
+        </Box>
         <CardContent>
           <Typography variant="h5" sx={{ mb: 2 }}>
             {title}
           </Typography>
-          <Typography variant="body2" color="text.secondary">
+          <Typography
+            variant="body2"
+            color="text.secondary"
+            sx={{ fontSize: "17px" }}
+          >
             {desc}
           </Typography>
         </CardContent>
